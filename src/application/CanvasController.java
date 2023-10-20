234151d3ee3cd18ce6f8779288eaeddad2e94e4a
beji <simone0443@outlook.com>
2023-10-20 18:15:42 +0200

    Added the Canvas controller and a clear button. You can now add nodes to the canvas and remove them from the canvas using the mouse buttons

---
+++ src/application/CanvasController.java
@@ -0,0 +1,53 @@
+package application;
+
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+public class CanvasController {
+	
+	@FXML
+	private Pane mainPane;
+	
+	public void click()
+	{
+		mainPane.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event)
+			{
+				if(event.getButton() == MouseButton.PRIMARY)
+				{
+					System.out.println("x: " + event.getSceneX());
+					System.out.println("Y: " + event.getSceneY());
+					
+					Circle c = new Circle();
+					c.setCenterX(event.getSceneX());
+					c.setCenterY(event.getSceneY());
+					c.setRadius(20);
+					c.setFill(Color.RED);
+					
+					mainPane.getChildren().add(c);
+				}
+				if(event.getButton() == MouseButton.SECONDARY)
+				{
+					mainPane.getChildren().remove(event.getTarget());
+					//System.out.println(event.getTarget());
+				}
+			}
+			
+		});
+	}
+	
+	public void clearCanvas()
+	{
+		ObservableList<Node> children = mainPane.getChildren();
+		
+		mainPane.getChildren().removeIf(child -> (child.getTypeSelector().equals("Circle")));
+	}
+}
---
+++ src/application/Main.java
@@ -0,0 +1,35 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage stage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			stage.setWidth(1580);
+			stage.setHeight(800);
+			stage.setResizable(false);
+			stage.setTitle("Floyd-Warshall Algorithm Visualizer");
+			stage.setScene(scene);
+			stage.show();
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
